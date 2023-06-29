17b8a891c8508456f73c1c6b91108aabc128fa48
BrunnoFardindeSouza <136929628+BrunnoFardindeSouza@users.noreply.github.com>
2023-06-29 10:33:46 -0300

    .

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let calcular = document.getElementById('calcular')
 let limpar = document.getElementById('limpar')
 let res = document.getElementById('msg')
 let res1 = document.getElementById('msg1')
+let res2 = document.getElementById('msg2')
 
 let peso = document.getElementById('peso')
 let altura = document.getElementById('altura')
@@ -11,8 +12,31 @@ calcular.addEventListener('click',()=>{
     }else{
         console.log(peso.value)
         res.style.display='block'
-        let imc = Number(peso.value)/(Number(altura.value ** 2)) 
+        let imc = Number(peso.value)/(Number(altura.value ** 2))
+        var PesoCerto = Number(peso.value)
         res1.innerHTML=`Seu indice de massa corporal é ${imc}`
+    //     if(PesoCerto >= 17.0 && PesoCerto <=18.4){
+    //         // Magreza grau 1
+    //         res2.innerHTML='Você faz parte da classfição : Magreza grau 1 '
+
+    //     }else if(PesoCerto >= 18.5 && PesoCerto <= 24.9){
+    //         // Adequado
+    //         res2.innerHTML='Você faz parte da classfição : Adequado '
+
+    //     }else if(PesoCerto >=25.0 && PesoCerto <=29.9){
+    //         // Sobrepeso
+    //         res2.innerHTML='Você faz parte da classfição : Sobrepeso '
+    //     }else{
+    //         // Obesidade
+    //         res2.innerHTML='Você faz parte da classfição : Obesidade '
+    //     }
+        setTimeout(()=>{
+            imc = 0
+            res1.innerHTML=''
+            res2.innerHTML=''
+            res.style.display='none'
+
+        },15000) 
 
     }
     
